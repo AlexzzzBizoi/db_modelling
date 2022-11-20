61167fc43a58b3103ae3304560a0feb9a257040f
alex.bizoi <bizoialex67@gmail.com>
2022-11-20 15:17:07 +0200

    SQL script for populatie_judet table

---
+++ populatie_judet.sql
@@ -0,0 +1,16 @@
+--create populatie_judet
+CREATE TABLE IF NOT EXISTS populatie_judet(
+	id SERIAL PRIMARY KEY NOT NULL,
+	year DATE UNIQUE NOT NULL,
+	value INTEGER NOT NULL,
+	judete_fid INTEGER NOT NULL
+);
+
+--AD DATA TO TABLE
+INSERT INTO populatie_judet(year,value,judete_fid) VALUES
+	('2000-11-20',500000,1),
+	('2001-11-20',600000,1),
+	('2002-11-20',700000,1);
+
+SELECT * FROM populatie_judet;
+
