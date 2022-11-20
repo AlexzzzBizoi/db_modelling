05f5f64855798979f39ec6d8b22680e591e1c297
alex.bizoi <bizoialex67@gmail.com>
2022-11-20 14:48:17 +0200

    SQL script for tip_parc table

---
+++ tip_parc.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS tip_parc(
+	id serial PRIMARY KEY NOT null,
+	type varchar(20) not null,
+	description varchar(400)
+);
+
+-- INSERT DATA INTO TABLE AFTER CREATE
+INSERT INTO tip_parc(type,description) VALUES
+					('Parc National','Arie protejata infiintata confrom regulament Romsilva'),
+					('Parc natural','Arie naturala protejata conform OUG Life Natura 2000'),
+					('Stiintifica', 'Rezervatie apartinand UVT, Departament Geografie'),
+					('Sit Natura 2000', 'Sit cartat si fundamentat conform Life Natura 2000 Project');
+
+					
+--UPDATE DATA IF REQUIRED
+UPDATE tip_parc SET
+	type='Rez stiintifica',
+	description='Rezervatie apartinand UVT, Departament Geografie & FMI'
+WHERE type='Stiintifica'
+	
+SELECT * FROM tip_parc;
